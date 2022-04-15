bba0f5ed076ac5f0d6d5f938c40e629cc6872fb7
samchen <bcsunrsbh@gmail.com>
2022-04-15 09:37:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <HelloWorld  msg="Welcome to Your  App" />
+  {{test}}
+
+  <FooBar :message="'hi foo'" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "./components/HelloWorld.vue";
+import FooBar from './components/FooBar.vue';
+@Options({
+  components: {
+    HelloWorld,
+    FooBar,
+  },
+})
+export default class App extends Vue {
+  test: number = 3;
+
+  created() {
+    console.log(this.test);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FooBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+     <div class="hello">{{ message }} --- {{ data }}---{{ bmi }}</div>
+     <button v-on:click="add()">submit</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, reactive, } from "vue";
+
+export default defineComponent({
+  props: {
+    message: String,
+  },
+  setup(props) {
+    const state = reactive({
+      height: 180,
+      weight: 80,
+    });
+    let data: number = 10;
+    console.log(props.message);
+    const bmi = computed(() => {
+      console.log('run111')
+      return state.weight / Math.pow(state.height / 100, 2);
+    });
+
+    return {
+      data,
+      bmi,
+      add(){
+        state.height++;
+      }
+    };
+  },
+});
+</script>
